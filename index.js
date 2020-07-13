4926641997ebf465e8ca7c7d2d5f4de7d6cccc95
Oswald Calvario <oswald.calvario@gmail.com>
2020-07-13 04:21:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+class Dog {
+	constructor(name, sex) {
+		this.name = name;
+		this.sex = sex;
+	}
+
+	speak() {
+		return `${this.name} says woof!`
+	}
+}
+
+class Cat {
+	constructor(name, sex) {
+		this.name = name;
+		this.sex = sex;
+	}
+
+	speak() {
+		return `${this.name} says meow!`
+	}
+}
+
+class Bird {
+	constructor(name, sex) {
+		this.name = name;
+		this.sex = sex;
+	}
+
+	speak() {
+		let statement;
+		this.sex !== 'male' ? statement = `${this.name} says squawk!` : statement = `It\'s me! ${this.name}, the parrot!`
+		return statement;
+	}
+}
